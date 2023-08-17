656c20da2c88377b98fdb63cf6e9a7c3719b27c0
SAURAV DHAKA <sauravdhaka456@gmail.com>
2023-08-17 17:55:41 +0530

    second

--- instagram-clone/src/components/Navbar.js
+++ instagram-clone/src/components/Navbar.js
@@ -1,59 +1,107 @@
-import React , {useContext} from 'react'
-import logo from '../img/logo.jpg';
-import '../css/Navbar.css'
-import {Link} from 'react-router-dom'
-import { LoginContext } from '../context/LoginContext';
-import { useNavigate } from 'react-router-dom';
+import React, { useContext } from "react";
+import logo from "../img/logo.jpg";
+import "../css/Navbar.css";
+import { Link } from "react-router-dom";
+import { LoginContext } from "../context/LoginContext";
+import { useNavigate } from "react-router-dom";
 
-export default function Navbar({login}) {
-  const navigate = useNavigate()
+export default function Navbar({ login }) {
+  const navigate = useNavigate();
 
-  const {setModalOpen} = useContext(LoginContext)
+  const { setModalOpen } = useContext(LoginContext);
 
-  const loginStatus = ()=>{
-    const token = localStorage.getItem('jwt')
-    if(login || token){
+  const loginStatus = () => {
+    const token = localStorage.getItem("jwt");
+    if (login || token) {
       return [
         <>
-        <Link to='/profile'>
+          <Link to="/profile">
             <li>Profile</li>
-            </Link>
-            <Link to='/createPost'>
+          </Link>
+          <Link to="/createPost">
             <li>Create Post</li>
-            </Link>
-            <Link to='/followingpost' style={{marginLeft:"20px"}}>
-              My Following 
-            </Link>
-            <Link to={''}>
-              <button className='primaryBtn' onClick={()=> setModalOpen(true)}>Log Out</button>
-            </Link>
-            
-            </>
-      ]
-    }
-    else{
+          </Link>
+          <Link to="/followingpost" style={{ marginLeft: "20px" }}>
+            My Following
+          </Link>
+          <Link to={""}>
+            <button className="primaryBtn" onClick={() => setModalOpen(true)}>
+              Log Out
+            </button>
+          </Link>
+        </>,
+      ];
+    } else {
       return [
         <>
-         <Link to = '/signup'> 
+          <Link to="/signup">
             <li>SignUp</li>
-            </Link>
-            <Link to='/signin'> 
+          </Link>
+          <Link to="/signin">
             <li>SignIn</li>
-            </Link>
-        </>
-      ]
+          </Link>
+        </>,
+      ];
     }
-  }
-
+  };
+  const loginStatusMobile = () => {
+    const token = localStorage.getItem("jwt");
+    if (login || token) {
+      return [
+        <>
+          <Link to="/">
+            <li>
+              <span className="material-symbols-outlined">home</span>
+            </li>
+          </Link>
+          <Link to="/profile">
+            <li>
+              <span className="material-symbols-outlined">account_circle</span>
+            </li>
+          </Link>
+          <Link to="/createPost">
+            <li>
+              <span className="material-symbols-outlined">add_box</span>
+            </li>
+          </Link>
+          <Link to="/followingpost" style={{ marginLeft: "20px" }}>
+            <li>
+              <span className="material-symbols-outlined">explore</span>
+            </li>
+          </Link>
+          <Link to={""}>
+            <li onClick={() => setModalOpen(true)}>
+              <span className="material-symbols-outlined">logout</span>
+            </li>
+          </Link>
+        </>,
+      ];
+    } else {
+      return [
+        <>
+          <Link to="/signup">
+            <li>SignUp</li>
+          </Link>
+          <Link to="/signin">
+            <li>SignIn</li>
+          </Link>
+        </>,
+      ];
+    }
+  };
   return (
-    <div className='navbar'>
-        <img src= {logo} className='img' style={{cursor:"pointer"}} onClick={()=>{
-          navigate('/')
-        }}/ >
-        <ul className='nav-menu'>
-           {loginStatus()}
-            
-        </ul>
+    <div className="navbar">
+      <img 
+      id="insta-logo"
+        src={logo}
+        className="img"
+        style={{ cursor: "pointer" }}
+        onClick={() => {
+          navigate("/");
+        }}
+      />
+      <ul className="nav-menu">{loginStatus()}</ul>
+      <ul className="nav-mobile">{loginStatusMobile()}</ul>
     </div>
-  )
+  );
 }
